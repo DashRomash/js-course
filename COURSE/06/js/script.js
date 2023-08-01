db98068d22ed06d5007308661f846be75f9ee219
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-08-02 00:38:35 +0300

    Add files via upload

--- COURSE/06/js/script.js
+++ COURSE/06/js/script.js
@@ -1,4 +1,4 @@
-// task1
+// task1 получилось вроде
 
 // let out1 = document.querySelector('.out-1');
 // for (let i = 0; i < 3; i++) {
@@ -9,15 +9,61 @@
 // }
 
 
-// task2
+// task2 не доделано
 
-let out2 = document.querySelector('.out-2');
-for (let i = 0; i < 4; i++) {
-    for (let k = 0; k < 3; k++) {
-        out2.innerHTML += "*_";
+// let out2 = document.querySelector('.out-2');
+// for (let i = 1; i < 4; i++) {
+//     out2.innerHTML += i + '<br>';// фигня с переносом
+//     for (let k = 0; k < 3; k++) {
+//         out2.innerHTML += "*_";
+//     }
+// }
+
+// task3 получилось
+
+// let out3 = document.querySelector('.out-3');
+// for (i = 0; i < 4; i++) {
+//     for (k = 0; k < 4; k++) {
+//         out3.innerHTML += "*_"
+//     }
+//     out3.innerHTML += "<br>"
+// }
+
+//task4 получилось
+
+// let out4 = document.querySelector('.out-4');
+// for (i = 1; i < 4; i++) {
+//     out4.innerHTML += i + "_";
+//     for (k = 1; k <= 5; k++) {
+//         out4.innerHTML += k + "*";
+//     }
+// }
+
+//task5 получилось
+
+// let out5 = document.querySelector('.out-5');
+// for (i = 1; i < 4; i++) {
+//     for (k = 1; k <= 6; k++) {
+//         if (k % 2 == 1) {
+//             out5.innerHTML += "1"
+//         }
+//         else {
+//             out5.innerHTML += "0"
+//         }
+//     }
+//     out5.innerHTML += "<br>"
+// }
+
+//task6 получилось
+
+let out6 = document.querySelector('.out-6');
+for (i = 1; i < 4; i++) {
+    for (k = 1; k < 2; k++) {
+        out6.innerHTML += "10x01x"
     }
-    out2.innerHTML += i;
+    out6.innerHTML += "<br>"
 }
+
 
 
 
