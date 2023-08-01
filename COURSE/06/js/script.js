d2aaf99c86e06ad74b33ef8c61e631d15b0a1a20
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-08-01 23:35:38 +0300

    Add files via upload

---
+++ COURSE/06/js/script.js
@@ -0,0 +1,42 @@
+// task1
+
+// let out1 = document.querySelector('.out-1');
+// for (let i = 0; i < 3; i++) {
+//     for (let k = 0; k < 3; k++) {
+//         out1.innerHTML += "*"
+//     }
+//     out1.innerHTML += '_'
+// }
+
+
+// task2
+
+let out2 = document.querySelector('.out-2');
+for (let i = 0; i < 4; i++) {
+    for (let k = 0; k < 3; k++) {
+        out2.innerHTML += "*_";
+    }
+    out2.innerHTML += i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
