5065ca3b74cc585455fb887239cc651ebf8248f9
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-08-07 01:20:23 +0300

    Add files via upload

---
+++ COURSE/08/js/script.js
@@ -0,0 +1,86 @@
+//TASK1
+
+// let button1 = document.querySelector('.b-1');
+// let out1 = document.querySelector('.out-1');
+// let k = 1;
+// function t1() {
+//     while (k < 50) {
+
+//         k++
+//         out1.innerHTML += `${k}_`;
+
+//     }
+// }
+// button1.onclick = t1();
+
+//TASK2         почему то кнопка не привязывается к функции,а просто работает при обновлении стр.
+
+// let button2 = document.querySelector('.b-2');
+// let out2 = document.querySelector('.out-2');
+// let k = 0;
+// function t2() {
+//     while (k < 46) {
+//         k = k + 2;
+//         // console.log(k);
+//         out2.innerHTML += `${k}_`;
+//     }
+// }
+// button2.onclick = t2();
+
+
+//TASK 3
+
+// let button3 = document.querySelector('.b-3');
+// let out3 = document.querySelector('.out-3');
+// let k = 25;
+// function t3() {
+//     while (k >= 7) {
+//         // console.log(k);
+//         out3.innerHTML += `${k}_`;
+//         k--
+//     }
+// }
+// button3.onclick = t3();
+
+
+//TASK 4
+
+// let button4 = document.querySelector('.b-4');
+// let out4 = document.querySelector('.out-4');
+// let k = 77;
+// function t4() {
+//     while (k >= 35) {
+//         out4.innerHTML += `${k}_`;
+//         k = k - 3;
+
+//     }
+// }
+// button4.onclick = t4();
+
+
+//TASK 5
+
+// let button5 = document.querySelector('.b-5');
+// let out5 = document.querySelector('.out-5');
+// let k = 0;
+// function t5() {
+//     while (k <= 17) {
+//         k++;
+//         if (k % 2 == 1) {
+//             out5.innerHTML += `${k}_*`;
+//         }
+//         else {
+//             out5.innerHTML += `${k}_**`;
+//         }
+//     }
+// }
+// button5.onclick = t5();
+
+
+
+
+
+
+
+
+
