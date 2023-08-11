5239a608bf7e3791f8f32544a68d5716145590aa
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-08-11 23:33:57 +0300

    Add files via upload

---
+++ COURSE/09/js/script.js
@@ -0,0 +1,25 @@
+let out1 = document.querySelector('.out-1');
+let button1 = document.querySelector('.b-1');
+function t1() {
+    out1.style.width = '200px';
+    out1.style.height = '90px';
+}
+button1.onclick = t1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
