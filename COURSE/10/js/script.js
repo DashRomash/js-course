dabcb7b5ac4f3e388d1549814d60b5455e10cc21
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-08-20 23:46:01 +0300

    Add files via upload

--- COURSE/10/js/script.js
+++ COURSE/10/js/script.js
@@ -6,7 +6,7 @@ button1.onclick = function t1() {
     out1.innerHTML = ar1;
 }
 
-//TASK2 не работает почему то
+//TASK2 
 let out2 = "";
 let button2 = document.querySelector('.b-2');
 let ar2 = ['privet', 3, 5, 7, false, 'hooray', true, true, 2];
@@ -15,10 +15,16 @@ button2.onclick = function t2() {
     for (let i = 0; i < ar2.length; i++) {
         out2 += ar2[i] + " ";
     }
+    document.querySelector('.out-2').innerHTML = out2;
 }
-document.querySelector('.out-2').innerHTML = out2;
 
-
+//TASK3
+let out3 = document.querySelector('.out-3');
+let button3 = document.querySelector('.b-3');
+let ar3 = ['privet', 3, 5, 7, false, 'hooray', true, true, 2];
+button3.onclick = function t3() {
+    out3.innerHTML = ar3.length;
+}
 
 
 
