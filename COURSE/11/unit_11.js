43b0597f0f9e8a574a06f4faaf1b7996ba4ec70b
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-08-23 00:27:55 +0300

    Add files via upload

--- COURSE/11/unit_11.js
+++ COURSE/11/unit_11.js
@@ -43,3 +43,51 @@ button3.onclick = function () {
 
 //TASK4
 let button4 = document.querySelector('.b-4');
+let in4 = document.querySelector('.i-4');
+let out4 = document.querySelector('.out-4');
+function f4() {
+    d1.push(in4.value);
+    showArr(d1, out4);
+}
+function showArr(arr, outputElement) {
+    outputElement.textContent = arr.join(', ');
+}
+button4.onclick = function () {
+    f4()
+}
+//TASK5
+let button5 = document.querySelector('.b-5');
+let in5 = document.querySelector('.i-5');
+let out5 = document.querySelector('.out-5');
+function f5() {
+    d1.unshift(in5.value);
+    showArr(d1, out5);
+}
+function showArr(arr, outputElement) {
+    outputElement.textContent = arr.join(', ');
+}
+button5.onclick = function () {
+    f5()
+}
+
+//TASK6
+let button6 = document.querySelector('.b-6');
+let in6 = document.querySelector('.i-6');
+let out6 = document.querySelector('.out-6');
+let d6 = ['test', 5, 12];
+function f6() {
+    let a = in6.value;
+    d6[d6.length - 1] = a;
+    showArr(d6, out6);
+}
+function showArr(arr, outputElement) {
+    outputElement.textContent = arr.join(', ')
+}
+button6.onclick = function () {
+    f6()
+}
+
+//TASK7
+let button7 = document.querySelector('.b-7');
+let in7 = document.querySelector('.i-7');
+let out7 = document.querySelector('.out-7');
