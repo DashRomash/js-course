4fd109836ff54ec87e54b02d3f0cd024cef2e607
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-09-04 23:26:14 +0300

    Add files via upload

---
+++ COURSE/14/unit_14.js
@@ -0,0 +1,4 @@
+//key  b28e938fe926b887562426062e17a6de;
+//example  https://api.openweathermap.org/data/2.5/weather?id=2172797&appid={API key}
+
+// moy https://api.openweathermap.org/data/2.5/weather?id=524925&appid={b28e938fe926b887562426062e17a6de}
