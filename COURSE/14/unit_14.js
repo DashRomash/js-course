05c69d5912d0855b39ce4f0e29c6fa23dc0b2181
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-09-05 01:50:58 +0300

    Add files via upload

--- COURSE/14/unit_14.js
+++ COURSE/14/unit_14.js
@@ -1,4 +1,25 @@
 //key  b28e938fe926b887562426062e17a6de;
-//example  https://api.openweathermap.org/data/2.5/weather?id=2172797&appid={API key}
+// moy https://api.openweathermap.org/data/2.5/weather?id=524925&appid=b28e938fe926b887562426062e17a6de
 
-// moy https://api.openweathermap.org/data/2.5/weather?id=524925&appid={b28e938fe926b887562426062e17a6de}
+const param = {
+    'url': 'https://api.openweathermap.org/data/2.5/',
+    'appid': 'b28e938fe926b887562426062e17a6de'
+}
+
+
+function getWeather() {
+    const cityID = document.querySelector('#city').value;
+    fetch(`${param.url}weather?id=${cityID}&units=metric&appid=${param.appid}`)
+        .then(weather => {
+            return weather.json();
+        }).then(showWeather);
+
+}
+function showWeather(data) {
+    console.log(data);
+    document.querySelector('.city-name').textContent = data.name;
+    document.querySelector('.temperature').innerHTML = Math.round(data.main.temp) + '&deg;';
+    document.querySelector('.sky-details').textContent = data.weather[0]['description'];
+}
+getWeather();
+document.querySelector('#city').onchange = getWeather;
