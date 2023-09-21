ac16b63c0f4b3f1622e916e1c64b7e83737aecb9
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-09-22 01:51:51 +0300

    Add files via upload

--- COURSE/19/unit_19.js
+++ COURSE/19/unit_19.js
@@ -120,63 +120,126 @@ document.querySelector('.div-7').addEventListener('contextmenu', function (event
     t7();
 })
 
-// Task 8 ============================================
+// Task 8 ================ я условие даже чесно говоря не поняла=  не понимаю, решена ли задача===========================
 /*  Дано checkbox .ch-8. Повесьте на него событие onchange при котором на документе отключается клик правой кнопкой мыши если checkbox выбран и отключает если не выбран. */
 
 function t8() {
-
+    if (document.querySelector('.ch-8').checked) {
+        document.event.preventDefault();
+        console.log('if')
+    }
+    else {
+        document.delete.event.preventDefault();
+        console.log('else')
+    }
 }
 
 // ваше событие здесь!!!
+document.querySelector('.ch-8').onchange = function (event) {
+    t8;
+}
 
-
-// Task 9 ============================================
+// Task 9 ============== кароче я добавила класс изображению, чтобы ухватиться за него это читерство.==============================
 /*  Дан блок .div-9. Внутри блока - изображение 1.png. При клике правой кнопкой мыши  - меняйте изображение на 2.png. Надеюсь вы догадаетесь изменить только src изображения? */
 
 function t9() {
+    console.log('в функцию зашла')
+    // let img9 = document.querySelector('.img-9');
+    // img9.setAttribute('src', 'img / 2.png');
 
+    let img = document.querySelector('img');// это вариант если класс не добавлять но он тоже не работает
+    img.setAttribute('src', ' 2.png');
 }
 
 // ваше событие здесь!!!
+document.querySelector('.div-9').oncontextmenu = function (event) {
+    t9();
+    event.preventDefault();
+}
 
-
-// Task 10 ============================================
+// Task 10 ================= не работает== ваабще чудеса происходят=========================
 /*  Дан блок .div-10. Внутри блока - изображение 1.png. При наведении мыши (mouseenter)  - меняйте изображение на 2.png. */
 
 function t10() {
+    console.log('в функцию зашла 10') //ok
+    let img = document.querySelector('img');
+    console.log(img)
+    img.setAttribute('src', '2.png');
 
+    //     let div10 = document.querySelector('.div-10');
+    //     console.log(div10)
+    //     div10.setAttribute('src', '2.png');
 }
 
 // ваше событие здесь!!!
-
-// Task 11 ============================================
+document.querySelector('.div-10').onmouseenter = function (event) {
+    t10();
+}
+// Task 11 ================== в консоли имг меняются, а сами картинки нет==========================
 /*  Дан блок .div-11. Внутри блока - изображение 1.png. При наведении мыши (mouseenter)  - меняйте изображение на 2.png. При уведении мыши - mouseleave - возвращайте исходное изображение. */
 
 function t11() {
+    console.log('в функцию зашла 11')
+    let img = document.querySelector('img');
+    console.log(img)
+    img.setAttribute('src', '2.png');
 
+}
+function t111() {
+    console.log('в функцию зашла 111')
+    let img = document.querySelector('img');
+    console.log(img)
+    img.setAttribute('src', '1.png');
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.div-11').onmouseenter = function (event) {
+    t11();
+}
+document.querySelector('.div-11').onmouseleave = function (event) {
+    t111();
+}
 // Task 12 ============================================
 /*  Дан блок .div-12. Добавьте на него событие mousedown - при нажатии кнопки мыши - добавляйте ему класс active. */
 
 // функцию пишите самостоятельно
-
+function t12() {
+    let div12 = document.querySelector('.div-12');
+    div12.classList.add('active');
+}
 // ваше событие здесь!!!
-
+document.querySelector('.div-12').onmousedown = function (event) {
+    t12();
+}
 
 // Task 13 ============================================
 /*  Дан блок .div-13. Добавьте на него событие mousedown - при нажатии кнопки мыши - добавляйте ему класс active. Добавьте ему событие mouseup - при отпускании мыши - удаляйте класс active. */
 
 // функцию пишите самостоятельно
+function t13() {
+    let div13 = document.querySelector('.div-13');
+    div13.classList.add('active');
+}
+function t131() {
+    let div13 = document.querySelector('.div-13');
+    div13.classList.remove('active');
+}
 // ваше событие здесь!!!
-
+document.querySelector('.div-13').onmousedown = function (event) {
+    t13();
+}
+document.querySelector('.div-13').onmouseup = function (event) {
+    t131();
+}
 
 // Task 14 ============================================
 /*  Дан блок .div-14. При нажатии кнопки b-14 добавляйте к нему событие onclick - которое, при клике добавляем блоку div-14 класс active. */
 
 function t14() {
+    let div14 = document.querySelector('.div-14');
+    div14.onclick = (event) => {
+        div14.classList.add('active');
+    }
 
 }
 document.querySelector('.b-14').onclick = t14;
@@ -186,19 +249,27 @@ document.querySelector('.b-14').onclick = t14;
 /*  Дан блок .div-15. Добавьте на него событие move. При каждом движении мыши увеличивайте число внутри на 1. */
 
 function t15() {
+    document.querySelector('.div-15').textContent = document.querySelector('.div-15').textContent++;
+    document.querySelector('.div-15').textContent++;
 
 }
 // ваше событие здесь!!!
-
+document.querySelector('.div-15').onmousemove = function (event) {
+    t15();
+}
 
 // Task 16 ============================================
 /*  Дан блок .div-16. Добавьте на него событие move. При каждом движении мыши увеличивайте ширину блока на 1px. */
 
-function t16() {
-
+function t16(e) {
+    let w = 75;
+    document.querySelector('.div-16').style.width = w + 1 + 'px';
+    w++;
 }
 // ваше событие здесь!!!
-
+document.querySelector('.div-16').onmousemove = function (event) {
+    t16();
+}
 // Task 17 ============================================
 /*  Дано две кнопки - b-17_on и b-17_off. Напишите фукнции t17On и t17Off которые включают и отключают событие move в задании 16. */
 
