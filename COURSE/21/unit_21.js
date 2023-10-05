db5fa63a17e814d87c7f19a4753ae8c31d612402
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-10-06 02:01:58 +0300

    Add files via upload

--- COURSE/21/unit_21.js
+++ COURSE/21/unit_21.js
@@ -40,26 +40,43 @@ function ThirdTouch(event) {
 // Task 4 ============================================
 /*  Создайте блок div-4. И кнопку b-4. При нажатии кнопки - добавляйте событие ontouchstart на блок div-4. При событии происходит вывод текста touch в out-4.  */
 
-function t4() {
+// document.querySelector('.b-4').addEventListener('click', click4);
+// function click4(event) {
+//     console.log("клик")
+//     document.querySelector('.div-4').addEventListener('ontouchstart', FourthTouch);
+// }
+// function FourthTouch(event) {
+//     console.log("тач")
+//     document.querySelector('.out-4').textContent += ' touch';
+// }
+
+document.querySelector('.b-4').onclick = function () {
+    document.querySelector('.div-4').ontouchstart = t4;
 
 }
+function t4() {
 
-// ваше событие здесь!!!
+    document.querySelector('.out-4').textContent += ' touch';
+}
+
+
 
 // Task 5 ============================================
 /*  Дана кнопка b-5. При ее нажатии очищайте событие ontouchstart на блоке div-4. */
-
+document.querySelector('.b-5').onclick = t5;
 function t5() {
+    document.querySelector('.div-4').ontouchstart = null;
 
-}
+};
+
 
 // ваше событие здесь!!!
 
 // Task 6 ============================================
 /*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
-
+document.querySelector('.div-4').addEventListener('touchend', t6);
 function t6() {
-
+    document.querySelector('.out-6').textContent = 'touched';
 }
 
 // ваше событие здесь!!!
@@ -67,9 +84,10 @@ function t6() {
 
 // Task 7 ============================================
 /*  Дан блок div-7. Добавьте событие touch, при срабатывании которого окрашивайте блок в красный цвет. */
-
+document.querySelector('.div-7').ontouchstart = t7;
 function t7() {
 
+    document.querySelector('.div-7').classList.add('active');
 }
 
 // ваше событие здесь!!!
@@ -77,8 +95,13 @@ function t7() {
 // Task 8 ============================================
 /*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
 
+let a8 = ['red', 'green', 'blue', 'orange', 'pink', 'yellow'];
+// let a8 = [red, green, blue, orange, pink, yellow];
+document.querySelector('.div-8').ontouchstart = t8;
 function t8() {
-
+    console.log('tut');
+    let randIndx = Math.floor(Math.random() * a8.length);
+    document.querySelector('.div-8').style.backgroundColor = a8[randIndx];
 }
 
 // ваше событие здесь!!!
@@ -86,9 +109,10 @@ function t8() {
 
 // Task 9 ============================================
 /* Дан блок div-9. Добавьте событие ontouch. Выводите количество одновременных касаний в out-9. */
-
-function t9() {
-
+document.querySelector('.div-9').ontouchstart = t9;
+function t9(event) {
+    console.log(event);
+    document.querySelector('.out-9').textContent = event.touches.length;
 }
 
 // ваше событие здесь!!!
@@ -96,17 +120,22 @@ function t9() {
 
 // Task 10 ============================================
 /*  Дан блок div-10. Добавьте на него событие touchmove. При срабатывании события - увеличивайте его ширину на 1. */
-
+let w = 75;
+document.querySelector('.div-10').ontouchmove = t10;
 function t10() {
-
+    console.log('tut');
+    document.querySelector('.div-10').style.width = w + 1 + 'px';
+    w++;
 }
 
 // ваше событие здесь!!!
 
 // Task 11 ============================================
 /*  Дан блок div-11. Добавьте на него событие touch. При срабатывании выводите радиус события radiusX, radiusY. */
-
-function t11() {
+document.querySelector('.div-11').ontouchstart = t11;
+function t11(event) {
+    console.log(event);
+    document.querySelector('.out-11').textContent = `clientX:${event.touches[0].clientX}, clientY:${event.touches[0].clientY}`;
 
 }
 
