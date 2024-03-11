eef0169009a6b3669f7324e51b84f5fe439140b3
DashRomash <118935565+DashRomash@users.noreply.github.com>
2024-03-11 23:45:17 +0300

    Add files via upload

--- 200/js/script.js
+++ 200/js/script.js
@@ -68,6 +68,17 @@ function Points(games) {
 
 
 
+//Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0..toExponential.
+//ex 5
+function sum(numbers) {
+    console.log('зашла')
+    if (Array.isArray(numbers)) {
+        return numbers.reduce((accum, current) => accum + current)
+    } else if (numbers.length === 0) {
+        return 0;
+    }
+    return numbers;
+}
 
 
 
